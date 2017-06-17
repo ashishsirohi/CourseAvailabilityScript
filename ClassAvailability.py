b6c4bff4d11127865e126ce2253ca6c142440630
Ashish <ashishsirohi@live.com>
2017-06-17 03:26:07 -0700

    status script added

--- ClassAvailability.py
+++ ClassAvailability.py
@@ -4,8 +4,60 @@ from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 import re
 import psycopg2
+import time
 
 #function to check the current status of the class
-def check_status():
+def check_status(course, term):
+    # driver inititalizaiton
+    print "Checking status for %s..." %course
+    url = "https://webapp4.asu.edu/catalog/classlist?k=%s&t=%s&e=all&hon=F" % (course, term)
+    driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
+
+    driver.set_window_size(1024, 768)
+    driver.get(url)
+    #time.sleep(0.5)
+
+    #Selecting All class option
+    x = driver.find_element_by_id('searchTypeAllClass')
+    x.click()
+
+    url = "https://webapp4.asu.edu/catalog/classlist?k=%s&t=%s&e=all&hon=F" % (course, term)
+    driver.get(url)
+    driver.save_screenshot("class.png") #Testing purpose
+
+    #getting page source
+    source = driver.page_source
+    soup = bs(source, "lxml")
+
+    #print source
+    print "Current Status:"
+    status = []
+    instructor = soup.find("td", {"class": "instructorListColumnValue"}).text
+    #inst = str(instructor).strip('\t\n')
+    status.append(str(instructor).strip('\t\n'))
+    course = soup.find("td", {"class": "titleColumnValue"}).text
+    status.append(str(course).strip('\t\n'))
+    seats = soup.find("td", { "class":"availableSeatsColumnValue" }).text
+    s = str(seats).strip('\t\n')
+    status.append(str(s.strip('\n').split('of')[0]).strip('\n'))
+
+    print status
+
+    return status
+
+    #instructor = str(instructor)
+    #instructor_out = re.findall(r'\">(?:(.|\n)*)<span>((.|\n)*?)(</span>)', instructor)
+
+    #print seats
+    #seats = str(seats)
+    #seats_count = re.findall(r'\">(\d+)(?:(.|\n)*)(of)(?:(.|\n)*>)(\d+)', seats)
+
+    #print course
+    #course = str(course)
+    #course_name = re.findall(r'\">(\d+)(?:(.|\n)*)', course)
+
+    #print str(instructor_out[0][1]).strip(r'\t\n')
+    #print course_name
+    #print seats_count[0][0]
 
     pass
--- asuclassnotifiermain.py
+++ asuclassnotifiermain.py
@@ -1,18 +1,27 @@
 import sys
 import os
 import psycopg2
-from classavailbility import check_status
+from ClassAvailability import check_status
+from sendnotification import send_notification
 
 #connect to databse
-def getOpenConnection(user='admin', password='postgres', dbname='postgres'):
+def getOpenConnection(user='admin', password='postgres', dbname='asuca'):
     return psycopg2.connect("dbname='" + dbname + "' user='" + user + "' host='localhost' password='" + password + "'")
 
 def main():
     con = getOpenConnection()
     cur = con.cursor()
-    cur.execute("Select courses from userinfo")
+    cur.execute("Select courseid, term from asuca_courses")
+    result = cur.fetchall()
+    courses = []
+    term = []
+    #for x in range(len(result)):
+     #   check_status(result[x][0], result[x][1])
 
-    pass
+    status = check_status(result[3][0], result[3][1])
+    if status[2] > 0:
+        print "Seat Available"
+        send_notification()
 
 
 if __name__=="__main__":
